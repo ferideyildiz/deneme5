02d3c5dbd4b9f842ddbbf806f81a70180974b6b3
Feride <celikfeza2@gmail.com>
2020-11-20 20:00:38 +0300

    feride branch

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,6 +1,8 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("07:12-birinci komut");
+        System.out.println("07:52-ikinci komut");
+        System.out.println("07:56-ucuncu komut");
 
     }
 }
